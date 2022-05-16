4278a7edfcb05af9b4a169c2ad62083a06e1e65d
AlicjaKasicka <alice55451@gmail.com>
2022-05-16 16:10:23 +0200

    move logic to endpoints

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: uvicorn main:app --host=0.0.0.0 --port=${PORT:-5000}
+web: uvicorn endpoints:app --host=0.0.0.0 --port=${PORT:-5000}
--- main.py
+++ main.py
@@ -1,11 +1,2 @@
-from fastapi import FastAPI
-
 class HerokuApp:
     app_url = "https://app-basics.herokuapp.com/"  # Fill your heroku app url here
-
-
-app = FastAPI()
-
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
