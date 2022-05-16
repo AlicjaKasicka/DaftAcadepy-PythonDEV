3bee182529e40f52587454b4c0104cad8cb3c4af
AlicjaKasicka <alice55451@gmail.com>
2022-05-16 16:59:06 +0200

    add method endpoints

--- endpoints.py
+++ endpoints.py
@@ -7,6 +7,27 @@ app = FastAPI()
 def root():
     return {"start": "1970-01-01"}
 
-@app.get("/method")
+@app.get("/method", status_code=200)
 def method():
-    return 0
+    return {"method": "GET"}
+
+@app.put("/method", status_code=200)
+def method():
+    return {"method": "PUT"}
+
+@app.options("/method", status_code=200)
+def method():
+    return {"method": "OPTIONS"}
+
+@app.delete("/method", status_code=200)
+def method():
+    return {"method": "DELETE"}
+
+@app.post("/method", status_code=201)
+def method():
+    return {"method": "POST"}
+
+
+
+
+    
